e454f86b50e1315102265468e0d88b4eb65a3224
Dots <65274190+Dotsenkoav@users.noreply.github.com>
2025-12-03 12:41:18 +0700

    feature(Program): добавлен основной класс
    - Добавлен класс Program
    - Добавлена точка входа в программу  Main()
    
    ВАЖНОЕ ИЗМЕНЕНИЕ: добавлена ветка master (проект VS 2022)

--- LaboratoryOOP/Program.cs
+++ LaboratoryOOP/Program.cs
@@ -1,4 +1,17 @@
 ﻿namespace Dotsenko.LaboratoryOOP
 {
-
+    /// <summary>
+    /// Главный класс программы
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
 }
