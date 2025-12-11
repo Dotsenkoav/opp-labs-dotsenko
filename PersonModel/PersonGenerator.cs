41764fdba572441f95e2cacaf38492cc70584b48
Dots <65274190+Dotsenkoav@users.noreply.github.com>
2025-12-11 11:47:53 +0700

    fix(Lab1): добавлен генератор случайных людей

---
+++ PersonModel/PersonGenerator.cs
@@ -0,0 +1,64 @@
+﻿using LaboratoryFirst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonModel
+{
+    //TODO: refactor +
+    /// <summary>
+    /// Создает случайного человека
+    /// </summary>
+    /// <returns>Объект класса Person со случайными значениями полей</returns>
+    public class PersonGenerator
+    {
+        //TOOD: refactor +
+        /// <summary>
+        /// Создает случайного человека
+        /// </summary>
+        /// <returns>Объект класса Person со случайными значениями полей</returns>
+        public static Person GetRandomPerson()
+        {
+            Random random = new Random();
+
+            string[] maleNames = ReadFile("Data/male_names.txt");
+            string[] femaleNames = ReadFile("Data/female_names.txt");
+            string[] lastNames = ReadFile("Data/lastnames.txt");
+
+            Sex sex = random.Next(2) == 0 ? Sex.Male : Sex.Female;
+            string firstName = sex == Sex.Male
+                ? maleNames[random.Next(maleNames.Length)]
+                : femaleNames[random.Next(femaleNames.Length)];
+
+            string lastName = lastNames[random.Next(lastNames.Length)];
+            if (sex == Sex.Female)
+            {
+                lastName += "а";
+            }
+
+            int age = random.Next(Person.MinAge, Person.MaxAge);
+
+            return new Person(firstName, lastName, age, sex);
+        }
+
+        /// <summary>
+        /// Метод считывания строк в файле
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <returns>Массив слов</returns>
+        private static string[] ReadFile(string path)
+        {
+            //TODO: rsdn +
+            if (!File.Exists(path))
+            {
+                return Array.Empty<string>();
+            }
+            return File.ReadAllLines(path)
+                .Where(value => !string.IsNullOrWhiteSpace(value))
+                .ToArray();
+        }
+    }
+}
+}
